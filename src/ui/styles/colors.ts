927fce4ad914104bdcd06c6fa797150518c2dff2
Romain Lagorsse <rlagorsse@betomorrow.com>
2021-09-06 16:10:40 +0200

    Adjust text colors

--- src/ui/styles/colors.ts
+++ src/ui/styles/colors.ts
@@ -2,6 +2,7 @@ export const colors = {
 	primary: "#ff3d00",
 	white: "#ffffff",
 	gray: "#979797",
-	textPrimary: "#364229",
+	textPrimary: "#364249",
 	textSecondary: "#333333",
+	textPlaceholder: "#657884",
 };
