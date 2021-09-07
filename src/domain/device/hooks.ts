95dab9c5918e77a5fd2ad289d00821bf1030ddbb
Tom Hohler <thohler@betomorrow.com>
2021-09-07 15:20:14 +0200

    autoconnect state

--- src/domain/device/hooks.ts
+++ src/domain/device/hooks.ts
@@ -3,6 +3,7 @@ import { useObservable } from "micro-observables";
 import { DeviceSetupState } from "./deviceService";
 
 export const useSetupState = () => useObservable(useServices().deviceService.setupState);
+export const useAutoConnectState = () => useObservable(useServices().deviceService.autoConnectState);
 export const useAccountLinked = () =>
 	useObservable(useServices().deviceService.setupState) === DeviceSetupState.FINISHED; // TODO Will probably change once we get users
 export const useScannedDevices = () => useObservable(useServices().deviceService.scannedDevices);
--- src/ui/components/spinner.tsx
+++ src/ui/components/spinner.tsx
@@ -2,8 +2,11 @@ import React, { useEffect, useRef } from "react";
 import { Animated, Easing } from "react-native";
 import { ChunkedCircle } from "./shapes/chunkedCircle";
 
+interface SpinnerProps {
+	size?: number;
+}
 // @refresh reset
-export const Spinner: React.FC = () => {
+export const Spinner: React.FC<SpinnerProps> = ({ size = 52 }) => {
 	const t = useRef(new Animated.Value(0)).current;
 
 	useEffect(() => {
@@ -15,7 +18,7 @@ export const Spinner: React.FC = () => {
 		<Animated.View
 			style={{ transform: [{ rotate: t.interpolate({ inputRange: [0, 1], outputRange: ["0deg", "360deg"] }) }] }}
 		>
-			<ChunkedCircle size={52} pathRatio={0.6} strokeWidth={3} />
+			<ChunkedCircle size={size} pathRatio={0.6} strokeWidth={3} />
 		</Animated.View>
 	);
 };
