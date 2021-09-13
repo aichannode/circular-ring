2d4c63d493a857d6e9fdf112c4574592b27d7893
Tom Hohler <thohler@betomorrow.com>
2021-09-13 15:17:04 +0200

    for refresh using scrollview on home

--- src/ui/screens/home/homeScreen.tsx
+++ src/ui/screens/home/homeScreen.tsx
@@ -1,22 +1,52 @@
-import { NavigationProp } from "@react-navigation/native";
-import React from "react";
+import { useServices } from "@core/services";
+import { useSyncState } from "@domain/ring/hooks";
+import { SyncState } from "@domain/ring/ringService";
+import React, { useCallback, useEffect, useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
 import styled from "styled-components/native";
 import { CirclesBanner } from "./circlesBanner";
 import { SyncBanner } from "./syncBanner";
 
-interface HomeScreenProps {
-	navigation: NavigationProp<{ [k: string]: unknown }>;
-}
-export const HomeScreen: React.FC<HomeScreenProps> = ({}) => {
+export const HomeScreen: React.FC = () => {
+	const syncState = useSyncState();
+	const { ringService } = useServices();
+	const [forceRefreshing, setForceRefreshing] = useState(false);
+
+	const forceRefresh = useCallback(() => {
+		if (syncState !== SyncState.NONE) {
+			return;
+		}
+		setForceRefreshing(true);
+		ringService.syncData();
+	}, [syncState, setForceRefreshing]);
+
+	useEffect(() => {
+		if (syncState !== SyncState.PREPARING) {
+			setForceRefreshing(false);
+		}
+	}, [syncState]);
+
 	return (
 		<Container>
 			<CirclesBanner />
 			<MargedSyncBanner />
+			<ScrollView
+				style={{ flex: 1 }}
+				refreshControl={
+					<RefreshControl
+						enabled={syncState === SyncState.NONE}
+						refreshing={forceRefreshing}
+						onRefresh={() => forceRefresh()}
+					/>
+				}
+			/>
 		</Container>
 	);
 };
 
-const Container = styled.View``;
+const Container = styled.View`
+	flex: 1;
+`;
 
 const MargedSyncBanner = styled(SyncBanner)`
 	margin: 10px;
