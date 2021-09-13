c7b7575cc78d471d6ccabd0f234646d4ce0160ed
Romain Lagorsse <rlagorsse@betomorrow.com>
2021-09-13 17:42:57 +0200

    Modify button layout in signup confirmation code screen

--- src/ui/components/buttons.tsx
+++ src/ui/components/buttons.tsx
@@ -10,6 +10,7 @@ interface CircularButtonProps {
 	onPress: () => void;
 	light?: boolean;
 }
+
 export const PrimaryButton: React.FC<CircularButtonProps> = ({ onPress, style, light, children }) => {
 	return (
 		<Pressable onPress={onPress} style={style}>
@@ -67,6 +68,14 @@ export const SecondaryBigButton: React.FC<CircularButtonProps> = ({ onPress, sty
 	);
 };
 
+export const SimpleTextButton: React.FC<CircularButtonProps> = ({ onPress, style, children }) => {
+	return (
+		<Pressable onPress={onPress} style={style}>
+			<SimpleTextButtonText>{children}</SimpleTextButtonText>
+		</Pressable>
+	);
+};
+
 const PrimaryButtonText = styled.Text<{ light?: boolean }>`
 	${textStyles.primary};
 	color: ${colors.white};
@@ -85,6 +94,12 @@ const PrimaryBigButtonText = styled.Text`
 const GrayButtonText = styled.Text`
 	${textStyles.primary};
 	color: ${colors.textPrimary};
+`;
+
+const SimpleTextButtonText = styled.Text`
+	${textStyles.primary};
+	color: ${colors.textPrimary};
+	text-decoration: underline;
 `;
 
 const PrimaryContent = styled(LinearGradient)<{ light?: boolean }>`
