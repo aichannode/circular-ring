a7b25782df014f376221a71b1623d9f4779df028
Romain Lagorsse <rlagorsse@betomorrow.com>
2021-09-23 17:18:23 +0200

    Fixes

--- src/core/utils.ts
+++ src/core/utils.ts
@@ -36,7 +36,10 @@ export function replaceInArray<T>(array: readonly T[], index: number, newValue: 
 }
 
 export function arrayFromRange(start: number, end: number, increment: number | undefined = 1): number[] {
-	return Array.from({ length: (end + increment - start) / increment }, (_, i) => i * increment + start);
+	return Array.from({ length: (end + increment - start) / increment }, (_, i) => {
+		// Correctly rounded at 0.01 precision
+		return +(i * increment + start).toFixed(2);
+	});
 }
 
 export function toServerDate(date: Date) {
