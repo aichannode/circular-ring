60d58563d6dbfcddbc9ade4c205755e59e736260
Mazen dk <dkhil.mazen@gmail.com>
2021-11-08 11:04:52 +0200

    add rename device

--- src/ui/screens/myRing/myRingScreen.tsx
+++ src/ui/screens/myRing/myRingScreen.tsx
@@ -6,27 +6,56 @@ import { PrimaryText } from "@ui/components/text";
 import { useI18n } from "@ui/i18n";
 import { Routes, useRoutesNavigation } from "@ui/navigation/routes";
 import { FactoryResetBottomSheet } from "@ui/screens/myRing/factoryResetBottomSheet";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Alert } from "react-native";
 import styled from "styled-components/native";
+import { Channel } from "@domain/device/channels";
+import { useObservable } from "micro-observables";
+import { NamedUserRing } from "@domain/ring/ring";
 
 export const MyRingScreen: React.FC = () => {
 	const { bleDeviceService } = useServices();
 	const { navigate } = useRoutesNavigation();
 	const { format } = useI18n();
-
+	const { ringManagementService } = useServices();
+	const userRings = useObservable(ringManagementService.userRings);
 	const factoryResetBottomSheetRef = useRef<CircularBottomSheetHandle>(null);
 
+	const [currentRing, setCurrentRing] = useState<NamedUserRing>(userRings[0]);
+	const renameAlert = () => {
+		Alert.prompt(format("manage_rings.ring.rename"), "", [
+			{
+				text: format("global.cancel"),
+				onPress: () => {},
+				style: "cancel",
+			},
+			{
+				text: format("global.edit"),
+				onPress: (newnName) => {
+					if (newnName && newnName !== ""){
+						bleDeviceService.write(`${Channel.RENAME}${newnName}`);
+						userRings.map((ring) => {
+							if (ring.id === currentRing.id) {
+								setCurrentRing({ ...ring, name: newnName });
+								return { ...ring, name: newnName };
+							}
+							return ring;
+						});
+					}
+				},
+			},
+		]);
+	};
+	useEffect(() => {}, [currentRing]);
 	return (
 		<Container>
 			<RingBatteryView size={140} detailed />
 			<StyledPrimaryText
 				onLongPress={() => {
-					bleDeviceService.write("RWF1S10");
-					Alert.alert("Data added");
+					renameAlert();
 				}}
 			>
-				My Ring
+				{userRings[0].name}
 			</StyledPrimaryText>
 			<InfoListItem
 				name={format("ring.manage")}
