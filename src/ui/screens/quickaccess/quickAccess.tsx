396a286981eea2213b0f0f64af9ea74c74e5aafd
Esteban LECLET <estebanleclet@gmail.com>
2021-11-10 17:21:48 +0100

    Drag and drop alomost fully fonctionnal need to disable drag when one item left in each cat

--- src/ui/screens/quickaccess/quickAccess.tsx
+++ src/ui/screens/quickaccess/quickAccess.tsx
@@ -1,30 +1,375 @@
 import { ScrollScreen } from "@ui/components/scrollScreen";
 import { useI18n } from "@ui/i18n";
 import { colors } from "@ui/styles/colors";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import LinearGradient from "react-native-linear-gradient";
+import { TouchableOpacity, Text, StyleSheet, View } from "react-native";
+import { DraxProvider, DraxView } from "react-native-drax";
 
 export const QuickAccess: React.FC = () => {
 	const { format } = useI18n();
+	const _quickAccess = [
+		{
+			title: format("quickaccess.sleeptitle"),
+			desc: format("quickaccess.sleepdesc"),
+			id: "sleep",
+		},
+		{
+			title: format("quickaccess.alarmtitle"),
+			desc: format("quickaccess.alarmdesc"),
+			id: "alarm",
+		},
+		{
+			title: format("quickaccess.calendartitle"),
+			desc: format("quickaccess.calendardesc"),
+			id: "calendar",
+		},
+	];
+
+	const _disabledQuickAccess = [
+		{
+			title: format("quickaccess.timertitle"),
+			desc: format("quickaccess.timerdesc"),
+			id: "timer",
+		},
+	];
+
+	const [dragged, setDragged] = useState(-1);
+	const [disabledDragged, setDisabledDragged] = useState(-1);
+
+	const [receiver, setReceiver] = useState(-1);
+	const [disabledReceiver, setDisabledReceiver] = useState(-1);
+	const [quickAccess, setQuickAccess] = useState(_quickAccess);
+	const [disabledQuickAccess, setDisabledQuickAccess] = useState(_disabledQuickAccess);
+
+	console.log("CIR-275 DRAGGED", dragged);
+	console.log("CIR-275 DISABLED", disabledDragged);
+
+	console.log(
+		"CIR-275 QuickAccess",
+		quickAccess.map((q) => q.id)
+	);
+	console.log(
+		"CIR-275 disabledQuickAccess",
+		disabledQuickAccess.map((q) => q.id)
+	);
 
 	return (
 		<Container>
-			<Description>{format("quickaccess.description")}</Description>
-			<Label>{format("quickaccess.displayed")}</Label>
-			<QuickAccessContainer colors={[colors.orangeGradientEnd, colors.orangeGradientStart]}>
-				<InnerContainer>
-					<Draggable source={require("@assets/images/group.png")}></Draggable>
-					<RightContainer>
-						<Title>{format("quickaccess.sleeptitle")}</Title>
-						<TileDesc>{format("quickaccess.sleepdesc")}</TileDesc>
-					</RightContainer>
-				</InnerContainer>
-			</QuickAccessContainer>
-			<Label>{format("quickaccess.hidden")}</Label>
+			<DraxProvider>
+				<Description>{format("quickaccess.description")}</Description>
+				<Label>{format("quickaccess.displayed")}</Label>
+				{quickAccess.map((tile, i) => {
+					return (
+						<>
+							{((0 == i && dragged !== -1 && dragged != i) || (disabledDragged !== -1 && i == 0)) && ( // FIRST RECEIVER
+								<DraxView
+									style={receiver === i ? styles.receiverfocus : styles.receiver}
+									onReceiveDragEnter={({ dragged: { payload } }) => {
+										console.log(`CIR-275  OnDragENterReceive ${payload}`);
+										setReceiver(i);
+									}}
+									onReceiveDragExit={({ dragged: { payload } }) => {
+										console.log(`CIR-275  DragExitReceive ${payload}`);
+										setReceiver(-1);
+									}}
+									onReceiveDragDrop={({ dragged: { payload } }) => {
+										console.log(`CIR-275  received ${payload}`);
+
+										// const beginning = quickAccess.slice(0, payload);
+										// const end = quickAccess.slice(payload + 1, quickAccess.length);
+										// const newArrayWithoutTile = [...beginning, ...end];
+
+										// const newBeginning = newArrayWithoutTile.slice(0, i);
+										// const newEnd = newArrayWithoutTile.slice(i, quickAccess.length);
+										// console.log("CIR-275 NEW BEGINGIN", newBeginning);
+										// console.log("CIR-275 NEW END", newEnd);
+
+										// setQuickAccess([...newBeginning, quickAccess[payload], ...newEnd]);
+										// setDragged(-1);
+
+										// console.log("CIR-275 Begin", beginning);
+										// console.log("CIR-275 End", end);
+
+										const isInQuickAccess = quickAccess.findIndex((el) => el.id === payload.tile.id);
+
+										if (isInQuickAccess !== -1) {
+											console.log("CIR-275 From QUICK ACCESS");
+											// const newBeginning = disabledQuickAccess.slice(0, i + 1);
+											// const newEnd = disabledQuickAccess.slice(i + 1, quickAccess.length);
+											// setDisabledQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+											// setQuickAccess(quickAccess.filter((t) => t.id != payload.tile.id));
+
+											const newArrayWithoutTile = quickAccess.filter((t) => t.id != payload.tile.id);
+
+											const newBeginning = newArrayWithoutTile.slice(0, i);
+											const newEnd = newArrayWithoutTile.slice(i, quickAccess.length);
+											console.log("CIR-275 NEW BEGINGIN", newBeginning);
+											console.log("CIR-275 NEW END", newEnd);
+											setQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+										} else {
+											console.log("CIR-275 From DISABLED ACCESS");
+											const newBeginning = quickAccess.slice(0, i);
+											const newEnd = quickAccess.slice(i, disabledQuickAccess.length);
+											console.log("CIR-275 NEW DISABLED ARRAY", [
+												...newBeginning,
+												disabledQuickAccess[payload.i],
+												...newEnd,
+											]);
+											setQuickAccess([...newBeginning, disabledQuickAccess[payload.i], ...newEnd]);
+											setDisabledQuickAccess(disabledQuickAccess.filter((t) => t.id != payload.tile.id));
+										}
+
+										setDragged(-1);
+										setDisabledDragged(-1);
+									}}
+								/>
+							)}
+							<DraxView
+								key={i}
+								onDragStart={() => {
+									setDragged(i);
+									setReceiver(-1);
+									setDisabledReceiver(-1);
+									console.log("CIR-275 start drag", i);
+								}}
+								onDragEnd={() => {
+									console.log("CIR-275 OnDragEnd");
+									setDragged(-1);
+								}}
+								onDragExit={() => {
+									console.log("CIR-275 OnDragExit");
+									setReceiver(-1);
+								}}
+								payload={{ tile, i }}
+								animateSnapback={false}
+							>
+								<QuickAccessContainer key={i} colors={[colors.orangeGradientEnd, colors.orangeGradientStart]}>
+									<InnerContainer>
+										<Draggable source={require("@assets/images/group.png")}></Draggable>
+										<RightContainer>
+											<Title>{tile.title}</Title>
+											<TileDesc>{tile.desc}</TileDesc>
+										</RightContainer>
+									</InnerContainer>
+								</QuickAccessContainer>
+							</DraxView>
+							{((dragged !== -1 && dragged != i && dragged != i + 1) || disabledDragged !== -1) && ( // SECOND REICEVIER
+								<DraxView
+									style={receiver === i + 1 ? styles.receiverfocus : styles.receiver}
+									onReceiveDragEnter={({ dragged: { payload } }) => {
+										console.log(`CIR-275  OnDragENterReceive ${payload}`);
+										// setDragged(i);
+										setReceiver(i + 1);
+									}}
+									onReceiveDragExit={({ dragged: { payload } }) => {
+										console.log(`CIR-275  DragExitReceive ${payload}`);
+										// setDragged(-1);
+										setReceiver(-1);
+									}}
+									onReceiveDragDrop={({ dragged: { payload } }) => {
+										console.log(`CIR-275  received ${payload}`);
+
+										const isInQuickAccess = quickAccess.findIndex((el) => el.id === payload.tile.id);
+
+										if (isInQuickAccess !== -1) {
+											console.log("CIR-275 From QUICK ACCESS");
+											// const newBeginning = disabledQuickAccess.slice(0, i + 1);
+											// const newEnd = disabledQuickAccess.slice(i + 1, quickAccess.length);
+											// setDisabledQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+											// setQuickAccess(quickAccess.filter((t) => t.id != payload.tile.id));
+
+											const newArrayWithoutTile = quickAccess.filter((t) => t.id != payload.tile.id);
+
+											const newBeginning = newArrayWithoutTile.slice(0, i + 1);
+											const newEnd = newArrayWithoutTile.slice(i + 1, quickAccess.length);
+											console.log("CIR-275 NEW BEGINGIN", newBeginning);
+											console.log("CIR-275 NEW END", newEnd);
+											setQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+										} else {
+											console.log("CIR-275 From DISABLED ACCESS");
+											const newBeginning = quickAccess.slice(0, i + 1);
+											const newEnd = quickAccess.slice(i + 1, disabledQuickAccess.length);
+											console.log("CIR-275 NEW DISABLED ARRAY", [
+												...newBeginning,
+												disabledQuickAccess[payload.i],
+												...newEnd,
+											]);
+											setQuickAccess([...newBeginning, disabledQuickAccess[payload.i], ...newEnd]);
+											setDisabledQuickAccess(disabledQuickAccess.filter((t) => t.id != payload.tile.id));
+										}
+
+										setDragged(-1);
+										setDisabledDragged(-1);
+
+										// const newArrayWithoutTile = quickAccess.filter((t) => t.id != payload.tile.id);
+
+										// const newBeginning = newArrayWithoutTile.slice(0, i + 1);
+										// const newEnd = newArrayWithoutTile.slice(i + 1, quickAccess.length);
+										// console.log("CIR-275 NEW BEGINGIN", newBeginning);
+										// console.log("CIR-275 NEW END", newEnd);
+										// setQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+										// setDragged(-1);
+									}}
+								/>
+							)}
+						</>
+					);
+				})}
+				<Label>{format("quickaccess.hidden")}</Label>
+				{disabledQuickAccess.map((tile, i) => {
+					return (
+						<>
+							{(disabledDragged !== -1 && disabledDragged != i + 1 && disabledDragged != i) || // THIRD REICEIVER
+								(dragged !== -1 && i == 0 && (
+									<DraxView
+										style={disabledReceiver === i ? styles.receiverfocus : styles.receiver}
+										onReceiveDragEnter={({ dragged: { payload } }) => {
+											console.log(`CIR-275  OnDragENterReceive ${payload}`);
+											// setDragged(i);
+											setDisabledReceiver(i);
+										}}
+										onReceiveDragExit={({ dragged: { payload } }) => {
+											console.log(`CIR-275  DragExitReceive ${payload}`);
+											// setDragged(-1);
+											setDisabledReceiver(-1);
+											setReceiver(-1);
+										}}
+										onReceiveDragDrop={({ dragged: { payload } }) => {
+											console.log(`CIR-275  received ${payload}`);
+
+											const isInQuickAccess = quickAccess.findIndex((el) => el.id === payload.tile.id);
+
+											if (isInQuickAccess !== -1) {
+												console.log("CIR-275 From QUICK ACCESS");
+												const newBeginning = disabledQuickAccess.slice(0, i);
+												const newEnd = disabledQuickAccess.slice(i, quickAccess.length);
+												setDisabledQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+												setQuickAccess(quickAccess.filter((t) => t.id != payload.tile.id));
+											} else {
+												console.log("CIR-275 From DiSABLED ACCESS");
+												const newBeginning = disabledQuickAccess.slice(0, i);
+												const newEnd = disabledQuickAccess.slice(i, disabledQuickAccess.length);
+												console.log(
+													"CIR-275 NEW DISABLED ARRAY",
+													[...newBeginning, disabledQuickAccess[payload.i], ...newEnd].map((q) => q.id)
+												);
+												setDisabledQuickAccess([...newBeginning, disabledQuickAccess[payload.i], ...newEnd]);
+											}
+
+											setDragged(-1);
+										}}
+									/>
+								))}
+							<DraxView
+								key={i}
+								onDragStart={() => {
+									// setDragged(i);
+									setDisabledDragged(i);
+									console.log("CIR-275 start drag", i);
+									setDisabledReceiver(-1);
+									setReceiver(-1);
+								}}
+								onDragEnd={() => {
+									console.log("CIR-275 OnDragEnd");
+									setDragged(-1);
+									setDisabledDragged(-1);
+								}}
+								onDragExit={() => {
+									console.log("CIR-275 OnDragExit");
+									// setDragged(-1);
+								}}
+								payload={{ tile, i }}
+								animateSnapback={false}
+							>
+								<QuickAccessContainer key={i} colors={[colors.orangeGradientEnd, colors.orangeGradientStart]}>
+									<InnerContainer>
+										<Draggable source={require("@assets/images/group.png")}></Draggable>
+										<RightContainer>
+											<Title>{tile.title}</Title>
+											<TileDesc>{tile.desc}</TileDesc>
+										</RightContainer>
+									</InnerContainer>
+								</QuickAccessContainer>
+							</DraxView>
+							{((disabledDragged !== -1 && disabledDragged != i && disabledDragged != i + 1) || dragged != -1) && (
+								<DraxView
+									style={disabledReceiver === i + 1 ? styles.receiverfocus : styles.receiver}
+									onReceiveDragEnter={({ dragged: { payload } }) => {
+										console.log(`CIR-275  OnDragENterReceive ${payload}`);
+										// setDragged(i);
+										setDisabledReceiver(i + 1);
+									}}
+									onReceiveDragExit={({ dragged: { payload } }) => {
+										console.log(`CIR-275  DragExitReceive ${payload}`);
+										// setDragged(-1);
+										setDisabledReceiver(-1);
+										setReceiver(-1);
+									}}
+									onReceiveDragDrop={({ dragged: { payload } }) => {
+										console.log(`CIR-275  received ${payload}`);
+
+										const isInQuickAccess = quickAccess.findIndex((el) => el.id === payload.tile.id);
+
+										if (isInQuickAccess !== -1) {
+											console.log("CIR-275 From QUICK ACCESS");
+											const newBeginning = disabledQuickAccess.slice(0, i + 1);
+											const newEnd = disabledQuickAccess.slice(i + 1, quickAccess.length);
+											setDisabledQuickAccess([...newBeginning, quickAccess[payload.i], ...newEnd]);
+											setQuickAccess(quickAccess.filter((t) => t.id != payload.tile.id));
+										} else {
+											console.log("CIR-275 From DISABLED ACCESS");
+											const newBeginning = disabledQuickAccess.slice(0, i + 1);
+											const newEnd = disabledQuickAccess.slice(i + 1, disabledQuickAccess.length);
+											console.log("CIR-275 NEW DISABLED ARRAY", [
+												...newBeginning,
+												disabledQuickAccess[payload.i],
+												...newEnd,
+											]);
+											setDisabledQuickAccess([...newBeginning, disabledQuickAccess[payload.i], ...newEnd]);
+										}
+
+										setDragged(-1);
+									}}
+								/>
+							)}
+						</>
+					);
+				})}
+			</DraxProvider>
 		</Container>
 	);
 };
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	draggable: {
+		width: "100%",
+		height: 100,
+		backgroundColor: "blue",
+	},
+	receiver: {
+		width: "100%",
+		height: 20,
+		backgroundColor: "#EEE",
+		borderWidth: 0.5,
+		borderColor: colors.gray,
+		borderRadius: 8,
+	},
+	receiverfocus: {
+		width: "100%",
+		height: 100,
+		backgroundColor: "#EEE",
+		borderWidth: 0.5,
+		borderColor: colors.gray,
+		borderRadius: 8,
+	},
+});
 
 const Title = styled.Text`
 	font-size: 18px;
@@ -74,6 +419,7 @@ const QuickAccessContainer = styled(LinearGradient)`
 	height: 84px;
 	width: 100%;
 	border-radius: 8px;
+	margin-vertical: 5;
 `;
 
 const Container = styled(ScrollScreen)`
