148001db4f2d22ab4dca0755d3557266e25a7030
Esteban LECLET <estebanleclet@gmail.com>
2021-11-26 13:58:12 +0100

    Added firmware success screen

--- src/ui/screens/myRing/ringFirmwareUpdate.tsx
+++ src/ui/screens/myRing/ringFirmwareUpdate.tsx
@@ -16,6 +16,7 @@ import { SecondaryText } from "@ui/components/text";
 import { BleDeviceService, UpdateState } from "@domain/device/bleDeviceService";
 import { useNavigation } from "@react-navigation/core";
 import { UserRing } from "@domain/ring/ring";
+import { Image } from "react-native";
 
 const startDFU = async (bleService: BleDeviceService) => {
 	await bleService.startDfuMode();
@@ -29,8 +30,8 @@ export const RingFirmwareUpdate: React.FC = () => {
 	const connectedRing = useObservable(bleDeviceService.connectedDevice);
 	const updateState = useObservable(bleDeviceService.updateState);
 
-	console.log("updateState", updateState);
-	console.log("CONNECTED RING", connectedRing);
+	// console.log("updateState", updateState);
+	// console.log("CONNECTED RING", connectedRing);
 
 	return (
 		<Container>
@@ -93,32 +94,43 @@ const UpdatingComponent: React.FC<I_NeedToUpdateComponent> = ({ showUpdateFailed
 
 	return (
 		<UpdatingContainer>
-			<Description>{format("updateFirmware.updating.description")}</Description>
-			<View style={{ marginTop: 80 }}>
-				<ChunkedCircle
-					size={140}
-					strokeWidth={12}
-					gradient={CircleGradient.PURPLE}
-					pathRatio={Math.round(progress + updateState.progress) / 100}
-				/>
-				<CenterView>
-					<BatteryValue style={{ fontSize: 35 }}>
-						{Math.round(progress + updateState.progress) ?? "?"}
-						{"%"}
-					</BatteryValue>
-				</CenterView>
-			</View>
-			<SecondaryText style={{ marginTop: 50 }}>{updateState.status}</SecondaryText>
-			{updateState.status === UpdateState.UPDATE_SUCCESS.status && (
-				<PrimaryButton
-					onPress={() => {
-						bleDeviceService.updateState.set(UpdateState.IDLE);
-						goBack();
-					}}
-					style={{ position: "absolute", bottom: "10%" }}
-				>
-					Back
-				</PrimaryButton>
+			{updateState.status === UpdateState.UPDATE_SUCCESS.status ? (
+				<>
+					<PrimaryText style={{ fontWeight: "bold", fontSize: 25, textAlign: "center", marginTop: 50 }}>
+						{format("updateFirmware.updateSuccess")}
+					</PrimaryText>
+					<CenterView>
+						<Image style={{ height: 100, width: 100, borderWidth: 1 }} source={require("@assets/images/check.png")} />
+					</CenterView>
+					<PrimaryButton
+						onPress={() => {
+							bleDeviceService.updateState.set(UpdateState.IDLE);
+							goBack();
+						}}
+						style={{ position: "absolute", bottom: "0%" }}
+					>
+						{format("global.done")}
+					</PrimaryButton>
+				</>
+			) : (
+				<>
+					<Description>{format("updateFirmware.updating.description")}</Description>
+					<View style={{ marginTop: 80 }}>
+						<ChunkedCircle
+							size={140}
+							strokeWidth={12}
+							gradient={CircleGradient.PURPLE}
+							pathRatio={Math.round(progress + updateState.progress) / 100}
+						/>
+						<CenterView>
+							<BatteryValue style={{ fontSize: 35 }}>
+								{Math.round(progress + updateState.progress) ?? "?"}
+								{"%"}
+							</BatteryValue>
+						</CenterView>
+					</View>
+					<SecondaryText style={{ marginTop: 50 }}>{updateState.status}</SecondaryText>
+				</>
 			)}
 		</UpdatingContainer>
 	);
@@ -199,7 +211,7 @@ const NeedToUpdateComponent: React.FC<I_NeedToUpdateComponent> = ({ connectedRin
 					}}
 					style={{ position: "absolute", bottom: "10%" }}
 				>
-					Update
+					{format("global.update")}
 				</PrimaryButton>
 			) : (
 				<View style={{ display: "flex", flexDirection: "row", position: "absolute", bottom: "10%" }}>
@@ -216,7 +228,7 @@ const NeedToUpdateComponent: React.FC<I_NeedToUpdateComponent> = ({ connectedRin
 							goBack();
 						}}
 					>
-						Back
+						{format("global.back")}
 					</PrimaryButton>
 				</View>
 			)}
