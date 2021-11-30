0244453f7471d4d5d491a98eb9455cd79d9de7e7
Daniel Neveux <d.neveux@warfog.gg>
2021-11-30 10:09:11 +0000

    fix: cannot read undefined value of token payload

--- src/ui/containers/IfAdmin.tsx
+++ src/ui/containers/IfAdmin.tsx
@@ -3,5 +3,5 @@ import React from "react"
 
 export function IfAdmin({children}: React.PropsWithChildren<Record<string, unknown>>) {
     const { cognitoAuthService: { payload }} = useServices()
-    return <>{payload.get()?.["cognito:groups"].some(groupName => groupName === "admin") && children}</>
+    return <>{payload.get()?.["cognito:groups"]?.some(groupName => groupName === "admin") && children}</>
 }
