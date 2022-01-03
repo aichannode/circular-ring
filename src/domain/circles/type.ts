dfa378ca1a89c34dfa148ed297536212f1a68438
Esteban LECLET <estebanleclet@gmail.com>
2022-01-03 16:14:05 +0100

    Change image on sleep mode, need to have the right image, and fix the fact that entire home screen rewrite

--- src/domain/circles/type.ts
+++ src/domain/circles/type.ts
@@ -2,13 +2,13 @@ import { WordingKey } from "src/wordings";
 import { Routes } from "@ui/navigation/routes";
 
 export type CircleEntity = {
-  id: number;
+	id: number;
 	route: Routes;
 	source: number;
+	sleepModeIcon: number;
 	key: WordingKey;
 	desc: WordingKey;
-	on: boolean,
-	type: string
-}
-
- 
+	on: boolean;
+	type: string;
+	canNavigateInSleepMode: boolean;
+};
