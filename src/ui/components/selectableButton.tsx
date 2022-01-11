22eec53d7a32490a918457f8f1f04391ff1a7b49
dagatsoin <danil_fransman@hotmail.com>
2022-01-11 17:04:21 +0100

    fix: gradient direction on selectable button

--- src/ui/components/selectableButton.tsx
+++ src/ui/components/selectableButton.tsx
@@ -31,8 +31,8 @@ export const SelectableButton: React.FC<SelectableButtonProps> = ({
 			<TertiaryBorder
 				colors={
 					pressed
-						? gradient.slice(0)
-						: gradient.slice(0).reverse()
+						? gradient.slice(0).reverse()
+						: gradient.slice(0)
 				}
 				start={{ x: 0, y: 1 }}
 				end={{ x: 1, y: 0.5 }}
