ecac4d23c4ca44e29bd0054a2ccbf1c5321c6e83
Esteban LECLET <estebanleclet@gmail.com>
2022-01-11 12:12:28 +0100

    Fix icons

--- src/domain/circles/circlesService.ts
+++ src/domain/circles/circlesService.ts
@@ -44,7 +44,7 @@ export class CirclesService {
 			desc: "home.circles.live.description",
 			route: Routes.Live,
 			source: require("@assets/images/circleLive.png"),
-			sleepModeIcon: require("@assets/images/ciclesAlarmSleeepmode.png"),
+			sleepModeIcon: require("@assets/images/Sleepmode.png"),
 			on: true,
 			type: "Wellness",
 			canNavigateInSleepMode: false,
