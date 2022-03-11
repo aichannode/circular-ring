6d83a5620785eb8020f93c77086071867c18171a
dagatsoin <danil_fransman@hotmail.com>
2022-03-11 15:09:59 +0100

    fix: sleep debt wrong metric

--- src/domain/measure/representation/hooks.ts
+++ src/domain/measure/representation/hooks.ts
@@ -303,12 +303,12 @@ export function createRepresentation(apiService: ApiService, model: MeasureModel
 					},
 					[MetricType.UserDailySleepDebt]: {
 						value: data[MetricType.UserDailySleepDebt] as number,
-						thresholdLow: (data[MetricType.UserDailyPercTimeToFallAsleepGoalMin] as number) ?? 0.8,
-						thresholdHigh: (data[MetricType.UserDailyPercTimeToFallAsleepGoalMax] as number) ?? 0.9,
+						thresholdLow: (data[MetricType.UserDailyPercSleepDebtGoalMin] as number) ?? 0.8,
+						thresholdHigh: (data[MetricType.UserDailyPercSleepDebtGoalMax] as number) ?? 0.9,
 						percent: data[MetricType.UserDailyPercSleepDebt] as number,
 						controlState: getScoreControlStates({
-							lowThreshold: (data[MetricType.UserDailyPercTimeToFallAsleepGoalMin] as number) ?? 0.8,
-							highThreshold: (data[MetricType.UserDailyPercTimeToFallAsleepGoalMax] as number) ?? 0.9,
+							lowThreshold: (data[MetricType.UserDailyPercSleepDebtGoalMin] as number) ?? 0.8,
+							highThreshold: (data[MetricType.UserDailyPercSleepDebtGoalMax] as number) ?? 0.9,
 							score: data[MetricType.UserDailyPercTimeToFallAsleep] as number,
 						}),
 					},
