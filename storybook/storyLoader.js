43d9f5c05cd94bd4212f508f7903a9322a030a7c
dagatsoin <danil_fransman@hotmail.com>
2022-03-22 14:48:13 +0100

    update: add story for pie chart

--- storybook/storyLoader.js
+++ storybook/storyLoader.js
@@ -5,11 +5,13 @@
 
 function loadStories() {
 	require("../src/ui/components/lineChart/LineChart.stories");
+	require("../src/ui/components/measure/dailyPieChart.stories");
 	require("../src/ui/components/stepChart/StepChart.stories");
 }
 
 const stories = [
 	"../src/ui/components/lineChart/LineChart.stories",
+	"../src/ui/components/measure/dailyPieChart.stories",
 	"../src/ui/components/stepChart/StepChart.stories",
 ];
 
