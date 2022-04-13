bf76aa7d4c0246685b98d96b3e4afe59261fbc13
lucas marandat <lucas.marandat@gmail.com>
2022-04-13 14:40:41 +0000

    fix(testflight): app freeze in release mode
    * Fix testflight app freeze
    
    CLOSE: [CIR-806](https://circularing.atlassian.net/browse/CIR-806)

--- storybook/index.js
+++ storybook/index.js
@@ -1,27 +1,40 @@
-import { getStorybookUI, configure, addDecorator } from "@storybook/react-native";
-import { withKnobs } from "@storybook/addon-knobs";
-
-import asyncStorage from "@react-native-async-storage/async-storage";
+import React from "react";
+import { Platform } from "react-native";
 import SplashScreen from "react-native-splash-screen";
 
-import { loadStories } from "./storyLoader";
+// This is a workaround to dynamically import the storybook UI dependencies based on the platform.
+let InternalStorybookUIRoot = () => null;
+const StorybookUIRoot = () => <InternalStorybookUIRoot />;
 
-import "./rn-addons";
+// CIR-806: Storybook UI is not working on ios in release mode. This is a workaround to make it work.
+if (Platform.OS !== "ios" || __DEV__) {
+	import("@storybook/react-native").then(({ getStorybookUI, addDecorator, configure }) =>
+		import("@storybook/addon-knobs").then(({ withKnobs }) =>
+			import("@react-native-async-storage/async-storage").then((asyncStorage) =>
+				import("./storyLoader").then(({ loadStories }) =>
+					import("./rn-addons").then(() => {
+						if (__DEV__) {
+							SplashScreen.hide();
+						}
 
-SplashScreen.hide();
+						// enables knobs for all stories
+						addDecorator(withKnobs);
 
-// enables knobs for all stories
-addDecorator(withKnobs);
+						// import stories
+						configure(() => {
+							loadStories();
+						}, module);
 
-// import stories
-configure(() => {
-	loadStories();
-}, module);
-
-// Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
-// To find allowed options for getStorybookUI
-const StorybookUIRoot = getStorybookUI({
-	asyncStorage,
-});
+						// Refer to https://github.com/storybookjs/react-native/tree/master/app/react-native#getstorybookui-options
+						// To find allowed options for getStorybookUI
+						InternalStorybookUIRoot = getStorybookUI({
+							asyncStorage,
+						});
+					})
+				)
+			)
+		)
+	);
+}
 
 export default StorybookUIRoot;
