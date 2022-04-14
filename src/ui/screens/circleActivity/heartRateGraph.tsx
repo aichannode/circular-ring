a04dbb621e09cb9950b3819c4c603f59e30fa2d4
farouk <farouk.abdelkrim@gmail.com>
2022-04-14 10:11:50 +0000

    update: change min/max label
    * change min max/label in Line Chart
    
    Close CIR-827

--- src/ui/screens/circleActivity/heartRateGraph.tsx
+++ src/ui/screens/circleActivity/heartRateGraph.tsx
@@ -19,6 +19,8 @@ type Props = {
 	selectedDay: ISODay;
 };
 
+const tooltipSize = { width: 40, height: 20 };
+
 export const HeartRateGraph: React.FC<Props> = observer(function HeartRateGraph({ selectedDay }: Props) {
 	const { format } = useI18n();
 	const [isLoading, setLoading] = useState(true);
@@ -99,6 +101,15 @@ export const HeartRateGraph: React.FC<Props> = observer(function HeartRateGraph(
 					yMax={yMax}
 					yMinIndex={yMinIndex}
 					yMaxIndex={yMaxIndex}
+					xAxisContentInset={15}
+					tooltipYMin={15}
+					tooltipYMax={-30}
+					tooltipSize={tooltipSize}
+					renderTooltip={(value) => (
+						<>
+							<Tag containerStyle={{ backgroundColor: colors.red, marginBottom: 4 }}>{`${value}`}</Tag>
+						</>
+					)}
 				/>
 				<View style={{ marginTop: 20 }}>
 					<GraphLegend
