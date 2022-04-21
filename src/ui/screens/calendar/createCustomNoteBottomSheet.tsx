146159405b774a36f09d16902056a14a1322f458
Esteban LECLET <estebanleclet@gmail.com>
2022-04-21 09:38:40 +0000

    fix: fixed a bottom sheet that was overlaped by keyboard on ios
    close: [CIR-837](https://circularing.atlassian.net/jira/software/projects/CIR/boards/1?selectedIssue=CIR-837)

--- src/ui/screens/calendar/createCustomNoteBottomSheet.tsx
+++ src/ui/screens/calendar/createCustomNoteBottomSheet.tsx
@@ -1,16 +1,16 @@
 import { useRepresentations } from "@core/representation";
 import { CalendarErrorContext } from "@domain/calendar/common/type";
+import { BottomSheetInput } from "@ui/components/bottomSheet/bottomSheetInput";
 import { PrimaryButton, TertiaryButton } from "@ui/components/buttons";
-import { Grow, ResponsiveCenterView, Row } from "@ui/components/layout";
+import { Grow, Row } from "@ui/components/layout";
 import { Spinner } from "@ui/components/spinner";
 import { MediumTitleText } from "@ui/components/text";
 import { useI18n } from "@ui/i18n";
 import { colors } from "@ui/styles/colors";
-import { textStyles } from "@ui/styles/textStyles";
 import { reaction } from "mobx";
 import { observer } from "mobx-react-lite";
 import React, { useEffect, useState } from "react";
-import { Image, View } from "react-native";
+import { View } from "react-native";
 import styled from "styled-components/native";
 import { ErrorMessage } from "../../components/errorMessage";
 
@@ -58,25 +58,15 @@ export const CreateCustomNoteBottomSheet: React.FC<CreateCustomNoteBottomSheetPr
 		}, []);
 
 		return (
-			<Container horizontalPadding={0}>
+			<Container>
 				<Title>{format("calendar.add_custom_note")}</Title>
 				<Grow />
-				<SearchWrapper>
-					<SearchInput
+					<NameInput
 						placeholder={format("calendar.add_custom_note")}
 						value={name}
 						onChangeText={setName}
 						autoFocus={true}
 					/>
-					{name.length > 0 && (
-						<CloseWrapper onPress={() => setName("")}>
-							<Image
-								style={{ tintColor: colors.textPrimary, width: 14, height: 13 }}
-								source={require("@assets/images/close.png")}
-							/>
-						</CloseWrapper>
-					)}
-				</SearchWrapper>
 				<Grow />
 				{!loading ? (
 					<>
@@ -106,42 +96,10 @@ export const CreateCustomNoteBottomSheet: React.FC<CreateCustomNoteBottomSheetPr
 	}
 );
 
-const SearchWrapper = styled(View)`
-	background-color: ${colors.white};
-	border-radius: 20px;
-	padding-left: 14px;
-	padding-right: 7px;
-	height: 38px;
-	margin: 6px 16px;
-	display: flex;
-	flex-direction: row;
-  	shadow-color: #000;
-	shadow-offset: {
-	width: 0px,
-	height: 2px,
-	};
-	shadow-opacity: 0.25px;
-	shadow-Radius: 3.84px;
-	elevation: 12;
+const NameInput = styled(BottomSheetInput)`
 `;
 
-const SearchInput = styled.TextInput`
-	${textStyles.primary};
-	flex: 1;
-	padding: 0px 10px 0px 10px;
-`;
-
-const CloseWrapper = styled.Pressable`
-	width: 28px;
-	height: 28px;
-	border-radius: 14px;
-	background-color: ${colors.lightgray};
-	align-items: center;
-	justify-content: center;
-	align-self: center;
-`;
-
-const Container = styled(ResponsiveCenterView)`
+const Container = styled(View)`
 	flex: 1;
 	justify-content: space-between;
 	align-items: center;
