858cfb13c86f3effa0722305560909d97227653b
Esteban LECLET <estebanleclet@gmail.com>
2022-04-26 10:24:01 +0200

    us(admin): added a cross for admin to bypass forced firmware update

--- src/ui/navigation/rootNavigator.tsx
+++ src/ui/navigation/rootNavigator.tsx
@@ -40,7 +40,12 @@ export const RootNavigator: React.FC = () => {
 	const currentRing: UserRing = userRings.filter((ring) => ring.connected)[0];
 	const lastFirmwareVersion = useObservable(ringApi.firmwareVersion);
 	const [useByPass, setByPass] = useState(false);
+	const [byPassForcedFirmwareUpdate, setByPassForcedFirmwareUpdate] = useState(false);
 	const updateState = useObservable(bleDeviceService.updateState);
+	console.log("lastFirmwareVersion", lastFirmwareVersion);
+	const {
+		cognitoAuthService: { payload },
+	} = useServices();
 
 	const isOnboardingDone = isAuthenticated && hasUser;
 	if (!isAuthenticated) {
@@ -71,10 +76,16 @@ export const RootNavigator: React.FC = () => {
 		);
 	}
 	if (
-		updateState.status !== UpdateState.IDLE.status ||
-		(currentRing && lastFirmwareVersion !== currentRing.firmware && lastFirmwareVersion)
+		(updateState.status !== UpdateState.IDLE.status ||
+			(currentRing && lastFirmwareVersion !== currentRing.firmware && lastFirmwareVersion)) &&
+		!byPassForcedFirmwareUpdate
 	) {
-		return <RingFirmwareUpdate></RingFirmwareUpdate>;
+		return (
+			<RingFirmwareUpdate
+				showCross={payload.get()?.["cognito:groups"]?.some((groupName) => groupName === "admin")}
+				setByPassForcedFirmwareUpdate={setByPassForcedFirmwareUpdate}
+			></RingFirmwareUpdate>
+		);
 	}
 
 	return isOnboardingDone || useByPass ? (
--- src/ui/screens/myRing/firmwareUpdate/ringFirmwareUpdate.tsx
+++ src/ui/screens/myRing/firmwareUpdate/ringFirmwareUpdate.tsx
@@ -5,14 +5,25 @@ import React from "react";
 import styled from "styled-components/native";
 import { IsUpToDate } from "./IsUpToDate";
 import { UpdatingComponent } from "./updatingComponent";
+interface RingFirmwareUpdateProps {
+	showCross?: boolean;
+	setByPassForcedFirmwareUpdate: (value: boolean) => void;
+}
 
-export const RingFirmwareUpdate: React.FC = () => {
+export const RingFirmwareUpdate: React.FC<RingFirmwareUpdateProps> = ({ showCross, setByPassForcedFirmwareUpdate }) => {
 	const { bleDeviceService } = useServices();
 	const connectedRing = useObservable(bleDeviceService.connectedDevice);
 	const updateState = useObservable(bleDeviceService.updateState);
 
 	return (
 		<Container>
+			{showCross && (
+				<CloseContainer>
+					<ClosePressable onPress={() => setByPassForcedFirmwareUpdate(true)}>
+						<CloseImage source={require("@assets/images/crossOrange.png")} />
+					</ClosePressable>
+				</CloseContainer>
+			)}
 			{updateState.status === UpdateState.IDLE.status ? (
 				<IsUpToDate connectedRing={connectedRing}></IsUpToDate>
 			) : (
@@ -27,3 +38,16 @@ const Container = styled.View`
 	align-items: center;
 	padding: 60px 0px;
 `;
+
+const CloseContainer = styled.View`
+	width: 100%;
+	display: flex;
+	flex-direction: row;
+	justify-content: flex-end;
+`;
+
+const ClosePressable = styled.TouchableOpacity`
+	margin: 0px 40px 40px 0px;
+`;
+
+const CloseImage = styled.Image``;
