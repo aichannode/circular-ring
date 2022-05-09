25e66c146505cf4226dd9308b3297f03c96d8b50
farouk <farouk.abdelkrim@gmail.com>
2022-05-09 13:10:44 +0000

    fix: selected point on LineChart
    * fix selected point on LineChart
    
    Close CIR-903

--- src/ui/components/lineChart/LineChart.tsx
+++ src/ui/components/lineChart/LineChart.tsx
@@ -310,8 +310,8 @@ export function LineChart({
 									valueTextSize: 0,
 									legend: false,
 									circleColors: !!shouldShowMarker
-										? lines.map((_, i) => {
-												if (i == selectedX) {
+										? lines.map(({ index }) => {
+												if (index == selectedX) {
 													return processColor("#333333");
 												}
 												return processColor(color);
