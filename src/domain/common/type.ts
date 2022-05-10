a86e771a81c672312ce5fda5cf109cea85aa2d6d
dagatsoin <danil_fransman@hotmail.com>
2022-05-10 10:37:48 +0200

    perf: dev time infinite saving

--- src/domain/common/type.ts
+++ src/domain/common/type.ts
@@ -6,10 +6,10 @@ type toFive = 0 | 1 | 2 | 3 | 4 | 5;
 type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 type MM = `0${oneToNine}` | `1${0 | 1 | 2}`;
 type DD = `${0}${oneToNine}` | `${1 | 2}${d}` | `3${0 | 1}`;
-export type ISOMonth = `${YYYY}-${MM}`;
-export type ISODay = `${ISOMonth}-${DD}`;
-export type ISODate = ISOMonth | ISODay;
-export type TZ = `+${toTwelve}:${toFive}${d}` | `-${toTwelve}:${toFive}${d}`;
+export type ISOMonth = string; //`${YYYY}-${MM}`;
+export type ISODay = string; //`${ISOMonth}-${DD}`;
+export type ISODate = string; //ISOMonth | ISODay;
+export type TZ = string; //`+${toTwelve}:${toFive}${d}` | `-${toTwelve}:${toFive}${d}`;
 
 // XXX: From https://stackoverflow.com/a/71700658/10203321
 export type Tuple<T, N extends number, R extends readonly T[] = []> = R["length"] extends N
