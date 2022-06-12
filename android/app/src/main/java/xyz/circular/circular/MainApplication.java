94dba95757adf6430c385a13522c41d820a581b9
AlexMog <alexmog@live.fr>
2022-06-12 14:15:57 +0200

    chore: using new groupid for android

--- android/app/src/main/java/xyz/circular/circular/MainApplication.java
+++ android/app/src/main/java/xyz/circular/circular/MainApplication.java
@@ -1,4 +1,4 @@
-package com.circular.circular;
+package xyz.circular.circular;
 
 import android.app.Application;
 import android.content.Context;
@@ -75,7 +75,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.circular.circular.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("xyz.circular.circular.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
