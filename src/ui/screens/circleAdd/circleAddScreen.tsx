389119d7976223575cc4b40edf4a671e8ee87c80
Esteban LECLET <estebanleclet@gmail.com>
2022-06-14 08:04:35 +0000

    fix: ux correction on add a circle screen
    modified button design, fixed padding problem
    
    close: CIR-999

--- src/ui/screens/circleAdd/circleAddScreen.tsx
+++ src/ui/screens/circleAdd/circleAddScreen.tsx
@@ -1,12 +1,13 @@
 import { useServices } from "@core/services";
+import { CircleEntity } from "@domain/circles/type";
 import { ScrollScreen } from "@ui/components/scrollScreen";
 import { useI18n } from "@ui/i18n";
 import { colors } from "@ui/styles/colors";
 import { useObservable } from "micro-observables";
 import React from "react";
-import { TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 import styled from "styled-components/native";
-import { CircleEntity } from "@domain/circles/type";
 import { mergeDefaultAndUserCirle } from "../business";
 
 function createViews(myArray: CircleEntity[]) {
@@ -18,16 +19,19 @@ function createViews(myArray: CircleEntity[]) {
 		return (
 			<BoxContainer key={circle.id}>
 				<InnerContainer>
-					<Draggable source={circlesService.getIcon(circle.icon)}></Draggable>
+					<CircleIcon source={circlesService.getIcon(circle.icon)}></CircleIcon>
 					<RightContainer>
 						<View style={{ flexDirection: "row", width: "100%" }}>
 							<Bold>{title}</Bold>
-							<TouchableOpacity
-								style={{ padding: 10, marginTop: -10 }}
-								onPress={() => circlesService.toggleCircle(circle.id)}
-							>
-								<Status>{circle.enabled ? "ON" : "OFF"}</Status>
-							</TouchableOpacity>
+							<Button onPress={() => circlesService.toggleCircle(circle.id)}>
+								<ButtonContainer
+									start={{ x: 0, y: 0.5 }}
+									end={{ x: 1, y: 0.5 }}
+									colors={circle.enabled ? [...colors.gradient.orange] : [colors.gray, colors.gray]}
+								>
+									<Status enabled={circle.enabled}>{circle.enabled ? "On" : "Off"}</Status>
+								</ButtonContainer>
+							</Button>
 						</View>
 						<View>
 							<Light>{format(circle.description)}</Light>
@@ -64,26 +68,37 @@ export const CircleAddScreen: React.FC = () => {
 	);
 };
 
+const Button = styled.TouchableOpacity`
+	padding: 6px;
+	margin-top: -10px;
+	margin-left: -10px;
+`;
+
+const ButtonContainer = styled(LinearGradient)`
+	padding: 1.5px;
+	border-radius: 19px;
+`;
+
 const Container = styled(ScrollScreen)`
 	padding-top: 0;
 	width: 100%;
 `;
 
-const BoxContainer = styled(View)`
+const BoxContainer = styled.View`
 	width: 100%;
 	border-radius: 8px;
-	overflow: hidden;
 	margin-bottom: 1px;
 `;
 
 const InnerContainer = styled.View`
 	flex-direction: row;
 	background-color: ${colors.lightgray};
-	align-items: center;
+	min-height: 110px;
 `;
 
 const RightContainer = styled.View`
 	flex: 1;
+	margin: 10px;
 	margin-right: 20px;
 `;
 
@@ -102,17 +117,21 @@ const TypeTile = styled.Text`
 	margin-vertical: 20px;
 `;
 
-const Status = styled.Text`
-	color: ${colors.gray};
+const Status = styled.Text<{ enabled: boolean }>`
+	color: ${(props) => (props.enabled ? colors.white : colors.gray)};
 	font-size: 14px;
 	text-align: right;
 	width: 100%;
+	background-color: ${(props) => (!props.enabled ? colors.lightgray : "transparent")};
+	padding: 4px 10px;
+	border-radius: 19px;
 `;
 
-const Draggable = styled.Image`
-	margin-vertical: 19px;
+const CircleIcon = styled.Image`
+	margin-vertical: 25px;
 	margin-left: 20px;
 	margin-right: 20px;
+	align-items: center;
 `;
 
 const Bold = styled.Text`
